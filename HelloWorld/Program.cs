651ad7b3bfa24fa521295545739d71bee45e8dd3
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2023-09-18 00:18:40 +0600

    Add c# datatype code

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, from C#!");
+
+int x = 5;
+double y = 4.5;
+string z = "Dhaka";
+bool isMarried = true;
+DateTime date = new DateTime(1997, 9, 2);
+char c = 'a';
+decimal d = 5.5;
+float f = 5.5f;
+short s = 30000;  // s < 32767
