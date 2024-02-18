62c24af25e0e904f18b65a8642e41e57fd32641c
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2024-02-18 16:13:58 +0600

    Structured for store CSharp codes

---
+++ Problem Solving/A. Team/A. Team/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberOfProblem = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for(int i = 0; i < numberOfProblem; i++)
+{
+    int isPossiblePetya = Convert.ToInt32(Console.ReadLine());
+    int isPossibleVasya = Convert.ToInt32(Console.ReadLine());
+    int isPossibleTonya = Convert.ToInt32(Console.ReadLine());
+
+    if (isPossiblePetya + isPossibleVasya + isPossibleTonya >= 2)
+        int count = count++;
+}
+Console.WriteLine(count);
