51f3594f4f7cd9a608d7b8f4cc43c1c686aa397a
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2024-03-27 17:13:46 +0600

    Added assignment - 1

---
+++ Anisul Islam PlayList/Assignment-1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Assignment-1");
+
+Console.WriteLine("Anisur Rahman Faysal");
+Console.WriteLine("I'm 27 year old");
+Console.WriteLine("I'm C# developer");
