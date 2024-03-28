59ff08fc7001ba8ecec9b0ab3d3283c326d0facb
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2024-03-28 21:45:05 +0600

    Assignment-2

---
+++ Anisul Islam PlayList/Assignment-2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Assignment-2\n\n");
+
+string name = "Apple Iphone 15";
+double price = 320.5;
+string category = "smart phone";
+bool avaliable = true;
+int sold = 100;
+
+Console.Write("Name: ");
+Console.WriteLine(name);
+
+Console.Write("Price: $");
+Console.WriteLine(price);
+
+Console.Write("Category: ");
+Console.WriteLine(category);
+
+Console.Write("Avaliable: ");
+Console.WriteLine(avaliable);
+
+Console.Write("sold: ");
+Console.WriteLine(sold);
