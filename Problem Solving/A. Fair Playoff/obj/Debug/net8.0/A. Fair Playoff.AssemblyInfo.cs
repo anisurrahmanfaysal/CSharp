9fa04451d0b5828dc04b83aa0fd6b20d83752816
AnisurRahmanFaysal <faysal.swe27@gmail.com>
2024-08-08 15:03:19 +0600

    Fair Playoff stat - 1

--- Problem Solving/A. Fair Playoff/obj/Debug/net8.0/A. Fair Playoff.AssemblyInfo.cs
+++ Problem Solving/A. Fair Playoff/obj/Debug/net8.0/A. Fair Playoff.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Fair Playoff")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c61ec1fa23f40c98984275936f9b40e45b402cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83745d767b4ead8c897d767bcc3299927291ca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Fair Playoff")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Fair Playoff")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Problem Solving/A. Sum of 2050/obj/Debug/net8.0/A. Sum of 2050.AssemblyInfo.cs
+++ Problem Solving/A. Sum of 2050/obj/Debug/net8.0/A. Sum of 2050.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A. Sum of 2050")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32b399bdd2691d97199c49b21961f7c0de9eacb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83745d767b4ead8c897d767bcc3299927291ca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("A. Sum of 2050")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A. Sum of 2050")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
