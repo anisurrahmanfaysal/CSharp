a9cff544c1044b467dfe1112f7c16f20c0e3af2e
Faysal_win <faysal.swe27@gmail.com>
2024-10-22 17:34:13 +0600

    22.10.2024

---
+++ DevSkillClass/StaticClass/Geometry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClass
+{
+    public class Geometry
+    {
+        private static readonly double pi;
+        public static double PI
+        {
+            get { return pi; }
+        }
+
+        static Geometry() 
+        { 
+            pi = 3.1416;
+        }
+        public static double CalculateCircleArea(double radius)
+        {
+            return PI * radius;
+        }
+
+        public static double CalculateRectangleArea(double width,double height) 
+        {
+            return width * height;
+        }
+    }
+}
---
+++ DevSkillClass/StaticClass/Program.cs
@@ -0,0 +1,4 @@
+﻿using StaticClass;
+
+double area = Geometry.CalculateCircleArea(23.5);
+Console.WriteLine(Geometry.PI);
