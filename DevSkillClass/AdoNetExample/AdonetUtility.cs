f991254fd3bfb06113bf735ec2a5c843cf630ffb
Faysal_win <faysal.swe27@gmail.com>
2024-10-26 15:36:42 +0600

    24.10.26

--- DevSkillClass/AdoNetExample/AdonetUtility.cs
+++ DevSkillClass/AdoNetExample/AdonetUtility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace AdoNetExample
 {
-    internal class AdonetUtility
+    public class AdonetUtility
     {
+        private readonly string _connectionString;
+        public AdonetUtility()
+        {
+            _connectionString = "Server = DESKTOP-5IJV2CO\\SQLEXPRESS; Database = CSharpB18; User Id = csharpb18; Password = 123456;Trust Server Certificate=True";
+        }
+        public void RunSql(string sql)
+        {
+            SqlConnection sqlConnection = new SqlConnection(_connectionString);
+        }
     }
 }
