5a32d1ee70d74c3dfbe0f0a250f73f78a75db0fd
Faysal_win <faysal.swe27@gmail.com>
2024-11-24 15:40:31 +0600

    C# Learning D-11.24.24

--- DevSkillClass/GenericsExample/Program.cs
+++ DevSkillClass/GenericsExample/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using GenericsExample;
+
+Coordinate<int, double> c = new Coordinate<int, double>();
+c.X = 5;
+c.Y = 9;
+
+BubbleSort bubbleSort1 = new BubbleSort();
+
+var result = bubbleSort1.Sort(new int[] {3,6,6,3,4,});
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
