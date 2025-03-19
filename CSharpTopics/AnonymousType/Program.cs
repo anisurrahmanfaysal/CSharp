f18e85721f9b6a6ac79270e4c8ee10a53f6baa6f
Faysal_win <faysal.swe27@gmail.com>
2025-03-19 23:09:30 +0600

    Anonymous object

--- CSharpTopics/AnonymousType/Program.cs
+++ CSharpTopics/AnonymousType/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Anonymous Type");
+
+object p = new { Name = "Faysal", Age = 27 };
+//Console.WriteLine(p.Name);
+
+var q = new { Name = "Faysal", Age = 27 };
+Console.WriteLine(q.Name);
