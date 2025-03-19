1bf76430e4ef60afa363929d19a127150ba6bdae
Faysal_win <faysal.swe27@gmail.com>
2025-03-19 16:38:22 +0600

    var tuple

---
+++ CSharpTopics/ParameterModifier/Geometry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterModifier
+{
+    public class Geometry
+    {
+        public double CalculateAverage(params double[] items) 
+        {
+            double sum = 0;
+            foreach (var item in items)
+            {
+                sum += item;
+            }
+            return sum / items.Length;
+        }
+
+        public void Calculate(ref int x)
+        {
+            Console.WriteLine(x);
+            x = 9;
+        }
+
+        public void Calculate2(in int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        public void Calculate3(out int x, out int y, out int z)
+        {
+            x = 9;
+            y = 8;
+            z = 7;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ CSharpTopics/ParameterModifier/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ParameterModifier;
+
+Console.WriteLine("Parameter Modifier");
+
+Geometry geometry = new Geometry();
+
+double[] items = [3.5, 4.4];
+geometry.CalculateAverage(items);
+geometry.CalculateAverage(3.3,4.4,5.5,.5);
+
+int m = 5;
+geometry.Calculate(ref m);
+Console.WriteLine(m);
+
+int x;
+int y;
+int z;
+geometry.Calculate3(out x, out y, out z);
+Console.WriteLine($"X: {x}, Y:{y}, Z:{z}");
