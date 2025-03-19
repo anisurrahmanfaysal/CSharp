7de55ae5cf6e66f2c600f2d88f67ecbb17015949
Faysal_win <faysal.swe27@gmail.com>
2025-03-19 19:52:07 +0600

    Tuple

--- CSharpTopics/TupleExample/Program.cs
+++ CSharpTopics/TupleExample/Program.cs
@@ -1,4 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Tuple example");
 
+(int age, string name) x = (27, "Faysal");
+Console.WriteLine(x.Item1);
+Console.WriteLine(x.Item2);
 
+Console.WriteLine(x.age);
+Console.WriteLine(x.name);
+
+(int, double) Calculate(int[] items)
+{
+    var sum = 0;
+    foreach (var item in items)
+    {
+        sum += item;
+    }
+    var average = (sum / items.Length);
+
+    return (sum, average);
+}
+
+(int, string)[] personalInfo = [(23, "Hasan"), (33, "Tareq")];
