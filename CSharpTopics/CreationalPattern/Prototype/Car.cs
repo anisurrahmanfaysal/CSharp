8034e96a594ea362d2746fe7cfaeb70f45e0ae86
Faysal_win <faysal.swe27@gmail.com>
2025-04-07 23:18:05 +0600

    Added Creational Pattern

---
+++ CSharpTopics/CreationalPattern/Prototype/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPattern.Prototype
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public double Fuel { get; set; }
+
+        public Car Copy() { 
+            return new Car { Fuel = Fuel, Name = Name };
+        }
+    }
+}
---
+++ CSharpTopics/CreationalPattern/Singleton/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPattern.Singleton
+{
+    public class Logger
+    {
+        private const string FILE_NAME = "";
+
+        private static Logger _logger;
+
+        private Logger() { }
+        public static Logger GetLogger()
+        {
+            if( _logger == null )
+                _logger = new Logger();
+            return _logger;
+        }
+        public void WriteLog(string message)
+        {
+
+            lock (this) { 
+                File.WriteAllText(FILE_NAME, message);
+            }
+        }
+    }
+}
