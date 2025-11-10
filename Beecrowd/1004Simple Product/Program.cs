8429d93eadbadccaeee0ed6c05457c9900ac63d9
Faysal_win <faysal.swe27@gmail.com>
2025-11-10 06:50:21 +0600

    Simple problem solved in beecrowed.

---
+++ Beecrowd/1004Simple Product/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+//void Multiply(int x, int y)
+//{
+//    Console.WriteLine("PROD = " + x * y);
+//}
+
+//Multiply(x, y);
+
+int PROD = x *  y;
+
+Console.WriteLine("PROD = " + x * y);
