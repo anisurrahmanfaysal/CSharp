c7677665b1df4761ee001408ba53d16a92564c2d
Faysal_win <faysal.swe27@gmail.com>
2025-11-13 18:17:21 +0600

    BankNote and coin solved in beecrowd

--- Beecrowd/1021BanknotesAndCoins/Program.cs
+++ Beecrowd/1021BanknotesAndCoins/Program.cs
@@ -1,53 +1,84 @@
-﻿var notes = double.Parse(Console.ReadLine());
+﻿var value = double.Parse(Console.ReadLine());
+int notes = (int)(value*100+.5);
+
+int count = 0;
+int[] banknotes = { 10000,5000,2000,1000,500,200};
+int[] coins = { 100, 50, 25, 10, 5, 1};
 
 Console.WriteLine("NOTAS:");
-
-double a = notes / 100;
-Console.WriteLine($"{a:F0} nota(s) de R$ 100.00");
-notes = notes % 100;
-
-a = notes / 50;
-Console.WriteLine($"{a:F0} nota(s) de R$ 50.00");
-notes = notes % 50;
-
-a = notes / 20;
-Console.WriteLine($"{a:F0} nota(s) de R$ 20.00");
-notes = notes % 20;
-
-a = notes / 10;
-Console.WriteLine($"{a:F0} nota(s) de R$ 10.00");
-notes = notes % 10;
-
-a = notes / 5;
-Console.WriteLine($"{a:F0} nota(s) de R$ 5.00");
-notes = notes % 5;
-
-a = notes / 2;
-Console.WriteLine($"{a:F0} nota(s) de R$ 2.00");
-notes = notes % 2;
+for (int i = 0; i < banknotes.Length; i++)
+{
+    count = (int)notes / banknotes[i];
+    Console.WriteLine($"{count:F0} nota(s) de R$ {banknotes[i]/100.0}.00");
+    notes %= banknotes[i];
+}
 
 Console.WriteLine($"MOEDAS:");
+for (int i = 0; i < coins.Length; i++)
+{
+    count = (int)(notes / coins[i]);
+    Console.WriteLine($"{count:F0} moeda(s) de R$ {coins[i]/100.0:F2}");
+    notes %= coins[i];
+}
 
-a = notes / 1;
-Console.WriteLine($"{a:F0} moeda(s) de R$ 1.00");
-notes = notes % 1;
 
-a = notes / .50;
-Console.WriteLine($"{a:F0} moeda(s) de R$ 0.50");
-notes = notes % .50;
 
-a = notes / .25;
-Console.WriteLine($"{a:F0} moeda(s) de R$ 0.25");
-notes = notes % .25;
 
-a = notes / .10;
-Console.WriteLine($"{a:F0} moeda(s) de R$ 0.10");
-notes = notes % .10;
 
-a = notes / .05;
-Console.WriteLine($"{a:F0} moeda(s) de R$ 0.10");
-notes = notes % .05;
 
-a = notes / .01;
-Console.WriteLine($"{a:F0} moeda(s) de R$ 0.10");
-notes = notes % .01;
+
+
+
+
+
+
+
+
+
+
+//double a = (int)notes / 100;
+//Console.WriteLine($"{a:F0} nota(s) de R$ 100.00");
+//notes = notes % 100;
+
+//a = (int)notes / 50;
+//Console.WriteLine($"{a:F0} nota(s) de R$ 50.00");
+//notes = notes % 50;
+
+//a = (int)notes / 20;
+//Console.WriteLine($"{a:F0} nota(s) de R$ 20.00");
+//notes = notes % 20;
+
+//a = (int)notes / 10;
+//Console.WriteLine($"{a:F0} nota(s) de R$ 10.00");
+//notes = notes % 10;
+
+//a = (int)notes / 5;
+//Console.WriteLine($"{a:F0} nota(s) de R$ 5.00");
+//notes = notes % 5;
+
+//a = (int)notes / 2;
+//Console.WriteLine($"{a:F0} nota(s) de R$ 2.00");
+//notes = notes % 2;
+
+//a = (int)notes / 1;
+//Console.WriteLine($"{a:F0} moeda(s) de R$ 1.00");
+//notes = notes % 1;
+
+//a = (int)(notes / .50);
+//Console.WriteLine($"{a:F0} moeda(s) de R$ 0.50");
+//notes = notes % .50;
+
+//a = (int)(notes / .25);
+//Console.WriteLine($"{a:F0} moeda(s) de R$ 0.25");
+//notes = notes % .25;
+
+//a = (int)(notes / .10);
+//Console.WriteLine($"{a:F0} moeda(s) de R$ 0.10");
+//notes = notes % .10;
+
+//a = (int)(notes / .05);
+//Console.WriteLine($"{a:F0} moeda(s) de R$ 0.10");
+//notes = notes % .05;
+
+//a = (int)(notes / .01);
+//Console.WriteLine($"{a:F0} moeda(s) de R$ 0.10");
